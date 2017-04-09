cfd885957c99c36f4eae314fe3b356957fb712b3
Kieran Thandi <Kieran.Thandi@bbc.co.uk>
2017-04-09 22:43:59 +0100

    p1 and p2

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # android-udacity
 "Nanodegree" in Android that I decided to take with Udacity - this is the place to see the projects I'm working on \o/
+
+### Project 1 ✔️ ###
+Creating a single screen app giving info about a small 'business'.
+
+### Project 2 ✔️ ###
+Implement an app to track scores between 2 teams. Don't ask me what the sport is ¯\_(ツ)_/¯
+
+### Project 3 ###
+Short quiz about a familiar topic.
+
+### Project 4 ###
+Musical structure app.
+
+### Project 5 ###
+Report card, model the info in a student report card.
+
+### Project 6 ###
+Tour guide app to naviagate a user around a city (should be fun considering my inability to read a map')
+
+### Project 7 ###
+Book listing ,simple search engine for books
+
+### Project 8 ###
+News app, displaying news stories using any API (lul)
+
+### Project 9 ###
+Habit tracker app
+
+### Project 10 ###
+Inventory app to track stuff from a retail stores, current stock ,supplier info etc.
---
+++ ScoreKeeperApp/app/src/main/java/kthand1/scorekeeperapp/MainActivity.java
@@ -0,0 +1,71 @@
+package kthand1.scorekeeperapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * The goal of this project is to create an Score Keeper app which gives a user the ability to keep track of the score of two different teams playing a game.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    public int scoreA = 0;
+    public int scoreB = 0;
+    public int point3 = 3;
+    public int point6 = 6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Calculates the score depending on which button is selected per team.
+     */
+    public void score(View view) {
+
+        if (view.getId() == R.id.teamA3) {
+            scoreA += point3;
+        } else if (view.getId() == R.id.teamA6) {
+            scoreA += point6;
+        } else if (view.getId() == R.id.teamB3) {
+            scoreB += point3;
+        } else if (view.getId() == R.id.teamB6) {
+            scoreB += point6;
+        }
+        displayScore("A", scoreA);
+        displayScore("B", scoreB);
+    }
+
+    /**
+     * Resets the score to 0.
+     */
+    public void reset(View view) {
+
+        scoreA = 0;
+        scoreB = 0;
+        displayScore("A", scoreA);
+        displayScore("B", scoreB);
+    }
+
+    /**
+     * Displays the score in the correct view.
+     */
+    private void displayScore(String team, int score) {
+        if (team == "A") {
+            TextView quantityTextView = (TextView) findViewById(R.id.team_a_score_view);
+            quantityTextView.setText("" + score);
+        } else {
+            if (team == "B") {
+                TextView quantityTextView = (TextView) findViewById(R.id.team_b_score_view);
+                quantityTextView.setText("" + score);
+            }
+        }
+    }
+
+}
+
+
